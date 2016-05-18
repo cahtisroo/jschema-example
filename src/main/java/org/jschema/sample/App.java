8255aecd0924d52359f29c7760c7e9a274f702e5
Sachin Patel <me@gizmosachin.com>
2016-05-18 11:41:56 -0700

    Use table for data

--- src/main/java/org/jschema/sample/App.java
+++ src/main/java/org/jschema/sample/App.java
@@ -3,13 +3,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.DecimalFormat;
 import java.net.URL;
 
 import static spark.Spark.*;
 import static spark.Spark.staticFileLocation;
 
 public class App {
-
     public static void main(String[] args) {
         staticFileLocation("public");
         port(getHerokuAssignedPort());
@@ -28,16 +28,25 @@ public class App {
                 YahooFinance.Query.Results.Quote quote = results.getquote();
 
                 String symbol = quote.getsymbol();
-                String askingPrice = quote.getAsk();
+                String price = quote.getAsk();
                 String change = quote.getChange();
-                resultsString = "<h2>" + symbol + "</h2><br>" + "<h2>" + askingPrice + " USD</h2><br>" + "<h2>Change: " + change + "</h2><br>";
+
+                String priceString = formatDoubleString(price);
+                String changeString = formatDoubleString(change.substring(1));
+
+                resultsString = "<table class=\"table\"><tr><td>Ticker</td><td>" + symbol + "</td></tr><td>Price</td><td>$" + priceString + "</td></tr><td>Change</td><td>" + change.charAt(0) + "$" + changeString + "</td></tr></table>";
             }  else {
                 ticker = "";
             }
 
             String header = "<html><title>JSchema Sample</title><script type=\"text/javascript\" src=\"main.js\" ></script><link href=\"bootstrap.css\" rel=\"stylesheet\" type=\"text/css\"><body onload=\"pageLoaded()\"><div class=\"container\"><h1 class=\"text-center\">JSchema Sample</h1><br><form id=\"form\"><input type='text' id='ticker-input' class='form-control input-lg' name='ticker' placeholder=\"Enter a stock ticker...\" value='";
-            return header + ticker + "'/></form><hr/><pre>" + resultsString + "</pre></div></body></html>";
+            return header + ticker + "'/></form><hr/>" + resultsString + "</div></body></html>";
         } );
+    }
+
+    private static String formatDoubleString(String doubleString) {
+        DecimalFormat df = new DecimalFormat("#.00");
+        return df.format(Double.parseDouble(doubleString));
     }
 
     private static String getContent(String urlStr) throws IOException {
@@ -65,5 +74,4 @@ public class App {
         }
         return 4567;
     }
-
 }
